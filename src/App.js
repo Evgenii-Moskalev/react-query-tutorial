8ef70dfa00a8a6ce93e5c82c87991cd5a5fdc52f
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2023-06-25 10:18:01 -0700

    React query set up

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 // import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import './App.css'
+import { QueryClientProvider, QueryClient, QueryClient } from 'react-query'
 import { HomePage } from './components/Home.page'
 import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
 import { SuperHeroesPage } from './components/SuperHeroes.page'
+import './App.css'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/super-heroes'>Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path='/super-heroes' element={<SuperHeroesPage />} />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/super-heroes'>Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/super-heroes' element={<SuperHeroesPage />} />
 
-          <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />} />
+            <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />} />
 
-          <Route path='/' element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
