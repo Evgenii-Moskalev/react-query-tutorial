9bafd0d28fc9e60035a2b20029a25763c9e83caa
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2023-07-03 21:30:58 -0700

    setting initial data for a query using queryClient/useQueryClient

--- src/hooks/useHeroData.js
+++ src/hooks/useHeroData.js
@@ -1,14 +1,28 @@
-import { useQuery } from 'react-query'
+import { useQuery, useQueryClient } from 'react-query'
 import axios from 'axios'
 
 const fetchHero = ({ queryKey }) => {
+    // console.log(queryKey);
     const heroId = queryKey[1]
-    return axios.get(`http://localhost:4000/superheroes/${heroId}`)
+    return axios.get(`http://localhost:3000/superheroes/${heroId}`)
 }
 
 // export const useHeroData = (heroId) => {
 //     return useQuery(['hero', heroId], () => fetchHero(heroId))
 // }
 export const useHeroData = (heroId) => {
-    return useQuery(['hero', heroId], fetchHero)
+    const queryClient = useQueryClient()
+    return useQuery(['hero', heroId], fetchHero, {
+        initialData: () => {
+            const hero = queryClient.getQueryData('super-heroes')?.data?.find(hero => hero.id === parseInt(heroId))
+        
+            if (hero) {
+                return {
+                    data: hero
+                }
+            } else {
+                return undefined;
+            }
+        }
+    })
 }
