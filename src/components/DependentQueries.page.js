af81bf2c3aeff056434d2cbf8fd5df345446138c
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2023-07-03 19:38:44 -0700

    React Query Dependent Queries rewriting

--- src/components/DependentQueries.page.js
+++ src/components/DependentQueries.page.js
@@ -1,28 +1,103 @@
-import { useQuery } from "react-query"
-import axios from "axios"
+// import { useQuery } from "react-query"
+// import axios from "axios"
+
+
+// const fetchUserByEmail = (email) => {
+//     // console.log(email);
+//     return axios.get(`http://localhost:4000/users/${email}`)
+// }
+
+// const fetchCoursesByChanelId = (channelId) => {
+//     return axios.get(`http://localhost:4000/channels/${channelId}`)
+// }
+
+// export const DependentQueriesPage = ({ email }) => {
+//     const { data: user } = useQuery(['user', email], () => fetchUserByEmail(email))
+//     const channelId = user?.data.channelId
+
+//     // console.log(!!channelId);
+//     const { data: courses } = useQuery(['courses', channelId], () => fetchCoursesByChanelId(channelId), {
+//         enabled: !!channelId
+//     })
+
+
+//     return (<>
+//         {/* <div>{channelId}</div> */}
+//         {courses?.data.courses.map(cours => <div key={cours}>{cours}</div>)}
+//     </>
+//     )
+// }
+
+// import { useQuery } from 'react-query';
+// import axios from 'axios';
+
+// const fetchUserByEmail = (email) => {
+//     return axios.get(`http://localhost:3000/users/${email}`);
+// }
+
+// const fetchCoursesByChanelId = (channelId) => {
+//     return axios.get(`http://localhost:3000/channels/${channelId}`)
+// }
+
+// export const DependentQueriesPage = ({ email }) => {
+
+//     const {isLoading, data: user, isError, error } = useQuery(['user', email], () => fetchUserByEmail(email));
+    
+//     const channelId = user?.data.channelId;
+
+//     const { data: courses } = useQuery(['courses', channelId], () => fetchCoursesByChanelId(channelId),
+//         { enabled: !!channelId }
+//     );
+
+
+//     if (isLoading) {
+//         return <h2>Loading...</h2>
+//     }
+//     if (isError) {
+//         return <h2>{error.message}</h2>
+//     }
+
+    
+    
+// // console.log(courses);
+
+//     return (<div>{courses?.data.courses.map(channel => <div key={channel}>{channel}</div>)}</div>)
+// }
+
+
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const fetchUserByEmail = (email) => {
     // console.log(email);
-    return axios.get(`http://localhost:4000/users/${email}`)
+    return axios.get(`http://localhost:3000/users/${email}`);
 }
 
 const fetchCoursesByChanelId = (channelId) => {
-    return axios.get(`http://localhost:4000/channels/${channelId}`)
+    return axios.get(`http://localhost:3000/channels/${channelId}`);
 }
 
-export const DependentQueriesPage = ({ email }) => {
-    const { data: user } = useQuery(['user', email], () => fetchUserByEmail(email))
-    const channelId = user?.data.channelId
+export const DependentQueriesPage = ({email}) => {
 
-    // console.log(!!channelId);
+
+    const{isLoading, data:user, isError, error } = useQuery(['user', email], () => fetchUserByEmail(email));
+
+    const channelId = user?.data.channelId;
+
     const { data: courses } = useQuery(['courses', channelId], () => fetchCoursesByChanelId(channelId), {
         enabled: !!channelId
-    })
+    });
 
+    if (isLoading) {
+        return (<h2>Loading...</h2>)
+    }
+    if (isError) {
+        return <h2>{error.message}</h2>
+}
 
-    return (<>
-        {/* <div>{channelId}</div> */}
-        <div>{courses.data.courses[0]}</div>
-    </>
+    // console.log(courses);
+
+    return (
+        <div>{courses?.data.courses.map(course => <div key={course}>{course}</div>)}</div>
     )
 }
