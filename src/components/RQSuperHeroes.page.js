c97dbbf7e8dc8ad067c5bbc03677354aa823cf49
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2023-07-07 20:52:30 -0700

    useMutation hook and axios.post

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -4,11 +4,15 @@
 // const fetchSuperHeroes = () => {
 //     return axios.get('http://localhost:4000/superheroes')
 // }
+import { useState } from "react";
+
 import { Link } from "react-router-dom";
 
-import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
+import { useSuperHeroesData, useAddSuperHeroData } from "../hooks/useSuperHeroesData";
 
 export const RQSuperHeroesPage = () => {
+    const [name, setName] = useState('');
+    const [alterEgo, setAlterEgo] = useState('');
 
     const onSuccess = (data) => {
         console.log('Perform side effect after data fetching', data);
@@ -54,6 +58,14 @@ export const RQSuperHeroesPage = () => {
 
     // console.log({ isLoading, isFetching });
 
+    const { mutate: addHero } = useAddSuperHeroData()
+
+    const handleAddHeroClick = () => {
+        // console.log({ name, alterEgo });
+        const hero = { name, alterEgo };
+        addHero(hero);
+    }
+
     if (isLoading || isFetching) {
         return (<h2>Loading...</h2>)
     }
@@ -64,6 +76,19 @@ export const RQSuperHeroesPage = () => {
 
     return (<>
         <h2>RQ Super Heroes Page</h2>
+        <div>
+            <input
+                type='text'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <input
+                type='text'
+                value={alterEgo}
+                onChange={(e) => setAlterEgo(e.target.value)}
+            />
+            <button onClick={handleAddHeroClick}>Add hero</button>
+        </div>
         <button onClick={refetch}>Fetch heroes</button>
         {
             data?.data.map(hero => {
